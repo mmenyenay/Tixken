e39bb2f7d2339b941b450fe0ac5b9a93363320fd
Ay <anyanimeakpan49@gmail.com>
2026-08-12 23:09:25 +0000

    Add static file serving middleware and public folder

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(express.static('public'));
 
 app.use('/api', require('./src/routes/events'));
 app.use('/api', require('./src/routes/tickets'));
